d0f000b4cb3b4c5e894d4e232e570778024fda91
Sebastian Bischoff <sebastian@salzreute.de>
2014-05-06 22:11:01 +0200

    initConnection-Methode hinzugefuegt
    initConnection() hinzugefuegt und programmiert

---
+++ pi-server/src/de/null_pointer/communication_pi/InitCommunicationPi.java
@@ -0,0 +1,38 @@
+package de.null_pointer.communication_pi;
+
+import lejos.pc.comm.NXTConnector;
+
+import org.apache.log4j.Logger;
+
+public class InitCommunicationPi {
+
+	private static Logger logger = Logger.getLogger(InitCommunicationPi.class);
+	private NXTConnector conn = new NXTConnector();
+
+	public CommunicationPi initConnection(String brick_id) {
+
+		try {
+
+			if (!conn.connectTo("usb://" + brick_id)) {
+
+				logger.warn("no NXT found using USB - simulation mode brick id "
+						+ brick_id);
+				return new VirtualCommunicationPi();
+
+			} else {
+				return new RealCommunicationPi(conn.getInputStream(),
+						conn.getOutputStream());
+			}
+
+		} catch (Exception e) {
+
+			logger.warn("PC: error connection to NXT: " + brick_id);
+			System.exit(0);
+			
+			return null;
+
+		}
+
+	}
+
+}
