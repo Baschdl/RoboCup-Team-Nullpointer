f529301473fd1d6afcbf5e98009a4b08db0efe9f
Sebastian Bischoff <sebastian@salzreute.de>
2014-05-06 21:46:55 +0200

    Grundgeruest von VirtualCommunicationPi hinzugefuegt

---
+++ pi-server/src/de/null_pointer/communication_pi/VirtualCommunicationPi.java
@@ -0,0 +1,23 @@
+package de.null_pointer.communication_pi;
+
+public class VirtualCommunicationPi extends Communication_Pi {
+
+	@Override
+	public String receiveString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sendString(String data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeConnection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
