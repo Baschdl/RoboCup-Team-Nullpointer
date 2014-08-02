a996ef7986b3117ecf95914eea3f0ed1f07a8554
Sebastian Bischoff <sebastian@salzreute.de>
2014-08-02 16:08:45 +0200

    Klasse PiServer hinzugefuegt
    Diese Klasse enthaelt die main-Methode und regelt die Wahl der
    Startparameter

---
+++ pi-server/src/PiServer.java
@@ -0,0 +1,17 @@
+public class PiServer {
+
+	public static void main(String[] args) {
+		for (String s : args) {
+			// comp steht für competition, führt das Wettkampfprogramm aus
+			if (s.equals("-comp")) {
+
+			}
+			// ruft die GUI auf
+			else if (s.equals("-gui")) {
+
+			}
+		}
+
+	}
+
+}
