6c6e32480b1e5c9b8dca2d44a234f097007bef38
Jan Krebes <jankrebes@gmx.de>
2014-08-02 20:53:11 +0200

    EOPDProcessingBrick hinzugefuegt

--- brick/src/de/null_pointer/sensorprocessing_brick/EOPDProcessingBrick.java
+++ brick/src/de/null_pointer/sensorprocessing_brick/EOPDProcessingBrick.java
@@ -1,5 +1,36 @@
 package de.null_pointer.sensorprocessing_brick;
 
-public class EOPDProcessingBrick {
+import de.null_pointer.communication_brick.BrickControlBrick;
+import lejos.nxt.ADSensorPort;
+import lejos.nxt.addon.EOPD;
 
+public class EOPDProcessingBrick {
+	
+	private EOPD eopd;
+	private BrickControlBrick brickControl;
+	private int sensorID;
+	
+	private int old_value = -1;
+	private int value = 0;
+	
+	public EOPDProcessingBrick(BrickControlBrick brickControl, int sensorID, ADSensorPort port, boolean longRange){
+		this.eopd = new EOPD(port, longRange);
+		this.brickControl = brickControl;
+		this.sensorID = sensorID;
+	}
+	
+	public void setLongRange(boolean longRange){
+		if(longRange){
+			eopd.setModeLong();
+		}else{
+			eopd.setModeShort();
+		}
+	}
+	
+	public void processData(){
+		if(this.old_value != (this.value = this.eopd.readRawValue())){
+			this.old_value = this.value;
+			brickControl.sendData(this.sensorID, 1, this.value);
+		}
+	}
 }
