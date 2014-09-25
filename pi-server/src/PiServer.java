0b75f5a3b3df4a230a8aa49170ec206708f1f3f2
Sebastian Bischoff <sebastian@salzreute.de>
2014-09-25 18:14:33 +0200

    Kommentare in der Klasse PiServer hinzugefuegt

--- pi-server/src/PiServer.java
+++ pi-server/src/PiServer.java
@@ -10,27 +10,32 @@ public class PiServer {
 	private static Logger logger = Logger.getRootLogger();
 
 	public static void main(String[] args) {
-
+		// Anlegen und Einrichten des Loggers
+		
 		// SimpleLayout layout = new SimpleLayout();
 		PatternLayout layout = new PatternLayout("%d{ISO8601} %-6p [%c] %m%n");
 		ConsoleAppender consoleAppender = new ConsoleAppender(layout);
 		logger.addAppender(consoleAppender);
 
 		// TODO: Abspeichern der Logs in ein File implementieren
+		
 		// boolean append = true;
 		// FileHandler handler = new FileHandler("default.log", append);
 		//
 		// Logger logger2 = Logger.getLogger(PiServer.class);
 		// logger.addHandler(handler);
 
+		// Spezifiziert welche Meldungen alles ausgegeben werden
 		// ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF:
 		logger.setLevel(Level.ALL);
 
 		logger.info("starting programm");
 
+		// Entscheidet anhand der Uebergabeparameter beim Start welche Programmteile ausgeführt werden
 		for (String s : args) {
 			// TODO: Aufruf des Wettkampfprogrammes implementieren, muss das
-			// immer ausgeführt werden?
+			// immer ausgeführt werden? Oder wollen wir die Möglichkeit haben auch nur die Werte auszugeben?
+			
 			// comp steht für competition, führt das Wettkampfprogramm aus
 			// if (s.equals("-comp")) {
 			//
