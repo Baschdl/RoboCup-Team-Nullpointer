27f428707748b8e7ee831d35c0f9b4755a1c89b5
Sebastian Bischoff <sebastian@salzreute.de>
2014-09-28 01:35:45 +0200

    Tests fuer invertOrientation() hinzugefuegt

--- pi-server/src/AllTests.java
+++ pi-server/src/AllTests.java
@@ -3,11 +3,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import de.null_pointer.communication_pi.BrickControlPiTest;
+import de.null_pointer.navigation.map.NodeTest;
 
 @RunWith(Suite.class)
 // In die geschweiften Klammern muessen weitere Testklassen mit Kommas getrennt
 // eingetragen werden
-@SuiteClasses({ BrickControlPiTest.class })
+@SuiteClasses({ BrickControlPiTest.class, NodeTest.class })
 public class AllTests {
 
 }
