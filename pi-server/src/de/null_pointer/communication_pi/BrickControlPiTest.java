9d8b1dcb4698070498be124791a8c7328aea2af7
Sebastian Bischoff <sebastian@salzreute.de>
2014-09-27 20:56:12 +0200

    Testfaelle fuer die checkString()-Methode hinzugefuegt

--- pi-server/src/de/null_pointer/communication_pi/BrickControlPiTest.java
+++ pi-server/src/de/null_pointer/communication_pi/BrickControlPiTest.java
@@ -6,9 +6,63 @@ import org.junit.Test;
 
 public class BrickControlPiTest {
 
+	BrickControlPi classUnderTest = new BrickControlPi();
+
 	@Test
-	public void testCheckString() {
-		fail("Not yet implemented");
+	public void testCheckStringCorrect() {
+		String stringToTest = "*123;123;123;123#";
+		float[] resultCorrect = { 123, 123, 123, 123 };
+		float[] result = classUnderTest.checkString(stringToTest);
+
+		assertArrayEquals(resultCorrect, result, 0);
+	}
+	
+	@Test
+	public void testCheckStringFalseOnlyHash() {
+		String stringToTest = "123;123;123;123#";
+		float[] result = classUnderTest.checkString(stringToTest);
+		
+		assertArrayEquals(null, result, 0);
+	}
+	
+	@Test
+	public void testCheckStringFalseOnlyStar() {
+		String stringToTest = "*123;123;123;123";
+		float[] result = classUnderTest.checkString(stringToTest);
+		
+		assertArrayEquals(null, result, 0);
+	}
+	
+	@Test
+	public void testCheckStringWithLetter() {
+		String stringToTest = "*123ab;1ac23;12dc3;123hb#";
+		float[] result = classUnderTest.checkString(stringToTest);
+
+		assertArrayEquals(null, result, 0);
+	}
+	
+	@Test
+	public void testCheckStringFalseTwoStars() {
+		String stringToTest = "**123;123;123;123#";
+		float[] result = classUnderTest.checkString(stringToTest);
+		
+		assertArrayEquals(null, result, 0);
+	}
+	
+	@Test
+	public void testCheckStringFalseTwoHashes() {
+		String stringToTest = "123;123;123;123##";
+		float[] result = classUnderTest.checkString(stringToTest);
+		
+		assertArrayEquals(null, result, 0);
+	}
+	
+	@Test
+	public void testCheckStringFalseCommaAsSeparator() {
+		String stringToTest = "*123,123,123,123#";
+		float[] result = classUnderTest.checkString(stringToTest);
+		
+		assertArrayEquals(null, result, 0);
 	}
 
 }
