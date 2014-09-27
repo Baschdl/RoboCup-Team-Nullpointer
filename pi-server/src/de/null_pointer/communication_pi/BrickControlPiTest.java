b59e962c4064b3de8695e535768267d81ee4502d
Sebastian Bischoff <sebastian@salzreute.de>
2014-09-27 20:23:16 +0200

    Klasse BrickControlPiTest zum Testen der Kommunikationsmethoden hinzugefuegt

---
+++ pi-server/src/de/null_pointer/communication_pi/BrickControlPiTest.java
@@ -0,0 +1,14 @@
+package de.null_pointer.communication_pi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BrickControlPiTest {
+
+	@Test
+	public void testCheckString() {
+		fail("Not yet implemented");
+	}
+
+}
