a6caee44b8a3de27f48d29b7c208308f77122922
Sebastian Bischoff <sebastian@salzreute.de>
2014-09-28 14:25:32 +0200

    Klassen aus Repository pi-testmodule uebernommen, da Abhaengigkeiten sonst nicht realisierbar sind

---
+++ pi-server/src/de/null_pointer/testmodules/testcommunication/TestBrickControlPiTest.java
@@ -0,0 +1,14 @@
+package de.null_pointer.testmodules.testcommunication;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBrickControlPiTest {
+
+	@Test
+	public void testResolveMotorport() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ pi-server/src/de/null_pointer/testmodules/testcommunication/TestCommunication.java
@@ -0,0 +1,11 @@
+package de.null_pointer.testmodules.testcommunication;
+
+import de.null_pointer.communication_pi.BrickControlPi;
+
+public class TestCommunication {
+	BrickControlPi conBrick = new BrickControlPi();
+	
+	public void main(String[] args) {
+		
+	}
+}
