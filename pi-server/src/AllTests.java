d198a98bd32c93c1657d15e67408f73f9cdf14a4
Sebastian Bischoff <sebastian@salzreute.de>
2014-09-28 18:19:21 +0200

    Initialisieren des Loggers bei Tests hinzugefuegt

--- pi-server/src/AllTests.java
+++ pi-server/src/AllTests.java
@@ -1,3 +1,8 @@
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -11,6 +16,7 @@ import de.null_pointer.behavior.VictimTest;
 import de.null_pointer.communication_pi.BrickControlPiTest;
 import de.null_pointer.navigation.map.NodeTest;
 import de.null_pointer.testmodules.testcommunication.TestBrickControlPiTest;
+import de.null_pointer.testmodules.testcommunication.TestCommunication;
 
 @RunWith(Suite.class)
 // In die geschweiften Klammern muessen weitere Testklassen mit Kommas getrennt
@@ -18,7 +24,22 @@ import de.null_pointer.testmodules.testcommunication.TestBrickControlPiTest;
 @SuiteClasses({ BrickControlPiTest.class, NodeTest.class,
 		TestBrickControlPiTest.class, MovingForwardTest.class,
 		BlackTileTest.class, IntersectionTest.class, NextTileTest.class,
-		SlopeTest.class, VictimTest.class })
+		SlopeTest.class, VictimTest.class, TestCommunication.class })
 public class AllTests {
 
+	private static Logger logger = Logger.getRootLogger();
+
+	@BeforeClass
+	public static void setUp() {
+		// Anlegen und Einrichten des Loggers
+
+		// SimpleLayout layout = new SimpleLayout();
+		PatternLayout layout = new PatternLayout("%d{ISO8601} %-6p [%c] %m%n");
+		ConsoleAppender consoleAppender = new ConsoleAppender(layout);
+		logger.addAppender(consoleAppender);
+
+		// Spezifiziert welche Meldungen alles ausgegeben werden
+		// ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF:
+		logger.setLevel(Level.INFO);
+	}
 }
