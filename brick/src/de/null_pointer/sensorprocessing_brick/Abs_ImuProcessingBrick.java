08115f16e05d03cc47f3e588d8a1081154b517ea
Sebastian Bischoff <sebastian@salzreute.de>
2014-12-02 16:30:03 +0100

    Klasse Abs_ImuProcessingBrick korrigiert und Versand der Daten hinzugefuegt

--- brick/src/de/null_pointer/sensorprocessing_brick/Abs_ImuProcessingBrick.java
+++ brick/src/de/null_pointer/sensorprocessing_brick/Abs_ImuProcessingBrick.java
@@ -8,47 +8,49 @@ public class Abs_ImuProcessingBrick {
 
 	private AbsoluteIMU_ACG abs_imu = null;
 	private BrickControlBrick brickControl;
-	
-	private int[] startGyro = new int[]{0,0,0};
-	private int[] gyro = new int[]{0,0,0};
+
+	private int[] startGyro = new int[] { 0, 0, 0 };
+	private int[] gyro = new int[] { 0, 0, 0 };
 	private int angle = 0;
 	private int angleExcludingPeriodicity = 0;
 	private int deltaAngle = 0;
-	
-	//TODO: Richtiger Wert?
+
+	// TODO: Richtiger Wert?
 	private double sensitivity = 0.00875;
-	
-	public Abs_ImuProcessingBrick(BrickControlBrick brickControl, I2CPort port){
+
+	public Abs_ImuProcessingBrick(BrickControlBrick brickControl, I2CPort port) {
 		abs_imu = new AbsoluteIMU_ACG(port);
 		startGyro = getGyro();
 		this.brickControl = brickControl;
 	}
-	
-	//TODO:
-	public void processData(){
+
+	// TODO:
+	public void processData() {
 		gyro = getGyro();
-		for(int i = 0; i < 3; i++){
+		for (int i = 0; i < 3; i++) {
+			// Nullpunktfehler korrigieren
 			gyro[i] -= startGyro[i];
+			deltaAngle += gyro[i] * sensitivity;
 		}
-		
-		for(int i = 0; i < 3; i++){
-			deltaAngle += gyro[i] + sensitivity;
-		}
-		//TODO: Zeitwert anpassen
+		// TODO: Zeitwert anpassen
 		angle += deltaAngle / 100;
-		
+
 		angleExcludingPeriodicity = angle % 360;
+
+		for (int i = 0; i < 3; i++) {
+			brickControl.sendData(5, 11 + i, gyro[i]);
+		}
 	}
-	
-	public int getFilter(){
+
+	public int getFilter() {
 		return abs_imu.getFilter();
 	}
-	
-	public int setFilter(int filterValue){
+
+	public int setFilter(int filterValue) {
 		return abs_imu.setFilter(filterValue);
 	}
-	
-	public int[] getGyro(){
+
+	public int[] getGyro() {
 		return abs_imu.getGyro();
 	}
 }
