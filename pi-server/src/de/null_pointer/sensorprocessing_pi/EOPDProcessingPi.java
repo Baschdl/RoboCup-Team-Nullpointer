7d3aaaec4500afe0f8e9525eec5aee459127a5ef
Jan <jankrebes@gmail.com>
2014-12-02 16:11:16 +0100

    kleine Anpassung an EOPDProcessingPi

--- pi-server/src/de/null_pointer/sensorprocessing_pi/EOPDProcessingPi.java
+++ pi-server/src/de/null_pointer/sensorprocessing_pi/EOPDProcessingPi.java
@@ -1,15 +1,14 @@
 package de.null_pointer.sensorprocessing_pi;
 
-
-
 public class EOPDProcessingPi {
 	double distance = -1;
 	
-	public double getEOPDdistance(){
+	public double getDistance(){
 		return distance;
 	}
 	
 	public void setEOPDdistance(int value){
+		//Wert zur Streckeneinteilung ggf. anpassen
 		distance = 30000 / Math.sqrt(value);
 	}
 }
