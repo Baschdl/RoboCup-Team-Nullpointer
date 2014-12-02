82996a87199662af85ea5e6adcebb195aef8a2ad
Jan <jankrebes@gmail.com>
2014-12-02 15:45:35 +0100

    EOPDProcessngPi implementiert

--- pi-server/src/de/null_pointer/sensorprocessing_pi/EOPDProcessingPi.java
+++ pi-server/src/de/null_pointer/sensorprocessing_pi/EOPDProcessingPi.java
@@ -1,5 +1,15 @@
 package de.null_pointer.sensorprocessing_pi;
 
-public class EOPDProcessingPi {
 
+
+public class EOPDProcessingPi {
+	double distance = -1;
+	
+	public double getEOPDdistance(){
+		return distance;
+	}
+	
+	public void setEOPDdistance(int value){
+		distance = 30000 / Math.sqrt(value);
+	}
 }
