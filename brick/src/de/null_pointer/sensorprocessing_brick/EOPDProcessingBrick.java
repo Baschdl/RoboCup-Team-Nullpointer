6615b9a529b994c9e2f09116d4fc064540aae29b
Jan <jankrebes@gmail.com>
2014-12-06 20:30:25 +0100

    Prozess der Mittelwertsbildung angepasst und Methodenbeschreibungen zu EOPDProcessingBrick hinzugefuegt

--- brick/src/de/null_pointer/sensorprocessing_brick/EOPDProcessingBrick.java
+++ brick/src/de/null_pointer/sensorprocessing_brick/EOPDProcessingBrick.java
@@ -10,15 +10,30 @@ public class EOPDProcessingBrick {
 	private BrickControlBrick brickControl;
 	private int sensorID;
 	
-	private int old_value = -1;
-	private int value = 0;
+	private int[] values = {0,0,0,0,0};
+	private int midValue = -1;
 	
+	/**
+	 * @param brickControl
+	 * 			Association to BrickControl
+	 * @param sensorID
+	 * 			ID of the EOPD sensor; decides wether it is the right or the left one
+	 * @param port
+	 * 			the port the Abs_Imu is plugged into
+	 * @param longRange
+	 * 			defines if the EOPD operates in longRange-mode
+	 */
 	public EOPDProcessingBrick(BrickControlBrick brickControl, int sensorID, ADSensorPort port, boolean longRange){
 		eopd = new EOPD(port, longRange);
 		this.brickControl = brickControl;
 		this.sensorID = sensorID;
 	}
 	
+	/**
+	 * changes the EOPDs mode to either long- or shortRange
+	 * @param longRange
+	 * 			true for longRange; false for shortRange
+	 */
 	public void setLongRange(boolean longRange){
 		if(longRange){
 			eopd.setModeLong();
@@ -27,10 +42,22 @@ public class EOPDProcessingBrick {
 		}
 	}
 	
+	/**
+	 * processes the sensor-readings, creates a middle value of the last 5 Readings and sends it if it has changed
+	 */
 	public void processData(){
-		if(old_value != (value = eopd.readRawValue())){
-			old_value = value;
-			brickControl.sendData(sensorID, 1, value);
+		for(int i = 0; i < 4; i++){
+			values[i] = values[i+1];
+		}
+		values[5] = eopd.readRawValue();
+		int buffer  = 0;
+		for(int i = 0; i < 5; i++){
+			buffer += values[i];
+		}
+		buffer /= 5;
+		if(buffer != midValue){
+			midValue = buffer;
+			brickControl.sendData(sensorID, 1, midValue);
 		}
 	}
 }
