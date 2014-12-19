fd584945bbb062a3d7244c4e00d546681f875788
lfkat <Scherer-Samuel@T-Online.de>
2014-12-19 22:02:30 +0100

    Virtuelle Sensoren weiterbearbeitet (LSA ueberarbeitet)

--- pi-server/src/de/null_pointer/testmodules/virtualhardware/VirtualDistNX.java
+++ pi-server/src/de/null_pointer/testmodules/virtualhardware/VirtualDistNX.java
@@ -2,4 +2,79 @@ package de.null_pointer.testmodules.virtualhardware;
 
 public class VirtualDistNX {
 
+	private int biggestValueNoSpace = 800;
+	private int smallestValueNoSpace = 100;
+	private int biggestValueOK = 800;
+	private int smallestValueOK = 100;
+	private final int amountValues = 20;
+	private int value[] = new int[amountValues];
+	private String distNX[] = new String[amountValues];
+	int range;
+	
+
+	/**
+	 * @param maxValue must not be bigger than 800
+	 * @param minValue has to be at least zero
+	 */
+	public VirtualDistNX(int maxValue, int minValue) {
+		
+		biggestValueNoSpace = maxValue;
+		smallestValueNoSpace = minValue;
+		smallestValueOK = biggestValueNoSpace + 1;
+		
+	}
+
+	private void generateValue(int mode) {
+		
+		if(mode == 0){
+			range = biggestValueOK - smallestValueOK;
+			value[0] = (int) Math.random() * range
+					+ smallestValueOK;
+			
+			for (int i = 1; i < amountValues; i++) {
+
+				value[i] = (int) Math.random() * 10 
+			}
+
+		}else if(mode == 1){
+			range = biggestValueNoSpace - smallestValueNoSpace;
+			
+			
+			value[0] = (int) Math.random() * range
+					+ smallestValueNoSpace;
+			
+			for (int i = 1; i < amountValues; i++) {
+
+				
+				
+
+			}
+		}
+	}
+
+	public String[] getOK() { // ok = mode 0
+		generateValue(0);
+
+		for (int i = 0; i < amountValues; i++) {
+
+			distNX[i] = "*1;1;" + value[i] + ";0#";
+
+		}
+
+		return distNX;
+
+	}
+
+	public String[] getNoSpaceInFront() { // getNoSpaceInFront = mode 1
+		generateValue(1);
+
+		for (int i = 0; i < amountValues; i++) {
+
+			distNX[i] = "*1;1;" + value[i] + ";0#";
+
+		}
+
+		return distNX;
+	}
+
 }
--- pi-server/src/de/null_pointer/testmodules/virtualhardware/VirtualLSA.java
+++ pi-server/src/de/null_pointer/testmodules/virtualhardware/VirtualLSA.java
@@ -1,60 +1,92 @@
 package de.null_pointer.testmodules.virtualhardware;
 
+import org.apache.log4j.Logger;
+
+
 public class VirtualLSA {
+	private static Logger logger = Logger.getLogger(VirtualLSA.class);
 	private int sensorNumber;
-	private String LSA[] = new String[8];
-	private int value[] = new int[8];
+	private final int amountValues = 20;
+	private String LSA[] = new String[amountValues];
+	private int value[] = new int[amountValues];
 	private int biggestValueWhite = 101;
 	private int smallestValueWhite = 93;
 	private int biggestValueBlack = 6;
 	private int smallestValueBlack = 0;
 
-	public VirtualLSA(int sensorN) {
+	public VirtualLSA(int sensorN, int maxValueWhite, int minValueWhite, int maxValueBlack, int minValueBlack) {
 
 		sensorNumber = sensorN;
+		biggestValueWhite = maxValueWhite;
+		smallestValueWhite = minValueWhite;
+		biggestValueBlack = maxValueBlack;
+		smallestValueBlack = minValueBlack;
 
 	}
 
-	private void generateValue(boolean black) {
+	private void generateValues(int mode) {
 		int rangeWhite = biggestValueWhite - smallestValueWhite;
 		int rangeBlack = biggestValueBlack - smallestValueBlack;
-		if (black == false) {
+		int rangeBetValues = smallestValueWhite - biggestValueBlack;
+		if (mode == 0) {
 
-			for (int i = 0; i < 8; i++) {
+			for (int i = 0; i < amountValues; i++) {
 
 				value[i] = (int) Math.random() * rangeWhite
 						+ smallestValueWhite;
 
 			}
-		} else {
-			for (int i = 0; i < 8; i++) {
+		} else if(mode == 1){
+			for (int i = 0; i < amountValues; i++) {
 
 				value[i] = (int) Math.random() * rangeBlack
 						+ smallestValueBlack;
 
 			}
 
+		}else if(mode == 2){
+			
+			for (int i = 0; i < amountValues; i++) {
+
+				value[i] = (int) Math.random() * rangeBetValues
+						+ biggestValueBlack;
+
+			}
+			
+		}else{
+			logger.error("mode > 2 - Invalid mode chosen!");
 		}
 
+	}
+
+	public String[] getBlack() {
+		generateValues(0);
+
+		for (int i = 0; i < amountValues; i++) {
+			LSA[i] = "*2;" + sensorNumber + ";" + value[i] + ";0#";
+		}
+		return LSA;
 	}
 
 	public String[] getWhite() {
-		generateValue(false);
+		generateValues(1);
 
-		for (int i = 0; i < 8; i++) {
-			LSA[i] = "*2;" + sensorNumber + value[i] + ";0#";
+		for (int i = 0; i < amountValues; i++) {
+			LSA[i] = "*2;" + sensorNumber + ";" + value[i] + ";0#";
 		}
 		return LSA;
+
 	}
-
-	public String[] getAllBlack() {
-		generateValue(true);
-
-		for (int i = 0; i < 8; i++) {
-			LSA[i] = "*2;" + sensorNumber + value[i] + ";0#";
+	
+	public String[] getValuesBetweenWhiteAndBlack(){
+		generateValues(2);
+		
+		for (int i = 0; i < amountValues; i++){
+			LSA[i] = "*2;" + sensorNumber + ";" + value[i] + ";0#";
 		}
+		
 		return LSA;
-
+		
 	}
 
 }
