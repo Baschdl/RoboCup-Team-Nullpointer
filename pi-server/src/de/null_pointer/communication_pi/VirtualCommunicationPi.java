6c7635efe72ab32ee552473aaa64cdb3d4ea8916
Sebastian Bischoff <sebastian@salzreute.de>
2014-12-20 01:19:41 +0100

    VirtualCommunicationPi erstellt

--- pi-server/src/de/null_pointer/communication_pi/VirtualCommunicationPi.java
+++ pi-server/src/de/null_pointer/communication_pi/VirtualCommunicationPi.java
@@ -1,7 +1,11 @@
 package de.null_pointer.communication_pi;
 
+import java.util.ArrayList;
+
+
 public class VirtualCommunicationPi extends CommunicationPi {
 	private boolean closed = false;
+	private ArrayList<String> dataArrayList = new ArrayList<String>();
 
 	public boolean isClosed() {
 		return closed;
@@ -9,20 +13,19 @@ public class VirtualCommunicationPi extends CommunicationPi {
 
 	@Override
 	public String receiveString() {
-		// TODO Auto-generated method stub
-		return null;
+		return dataArrayList.remove(0);
 	}
 
 	@Override
 	public void sendString(String data) {
-		// TODO Auto-generated method stub
+		dataArrayList.add(data);
 		
 	}
 
 	@Override
 	public void closeConnection() {
 		closed = true;
-		
+		dataArrayList = null;
 	}
 
 }
