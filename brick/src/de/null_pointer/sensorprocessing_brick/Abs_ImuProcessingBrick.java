23acb87119463c98c98064fb14e4f40bb338727d
Jan <jankrebes@gmail.com>
2014-12-20 22:38:01 +0100

    kleine Optimierungen an Abs_ImuProcessinBrick

--- brick/src/de/null_pointer/sensorprocessing_brick/Abs_ImuProcessingBrick.java
+++ brick/src/de/null_pointer/sensorprocessing_brick/Abs_ImuProcessingBrick.java
@@ -9,23 +9,23 @@ public class Abs_ImuProcessingBrick {
 	private AbsoluteIMU_ACG abs_imu = null;
 	private BrickControlBrick brickControl;
 
-	private int[] startGyro = new int[] {0,0,0};
-	private int[] gyro = new int[] {0,0,0};
-	private int[] angle = new int[]{0,0,0};
+	private int[] startGyro = new int[] { 0, 0, 0 };
+	private int[] gyro = new int[] { 0, 0, 0 };
+	private int[] angle = new int[] { 0, 0, 0 };
 	private int angleExcludingPeriodicity = 0;
-	private int[] oldAngleExcludingPeriodicity = new int[]{0,0,0};
-	private int[] deltaAngle = new int[]{0,0,0};
+	private int[] oldAngleExcludingPeriodicity = new int[] { 0, 0, 0 };
+	private int[] deltaAngle = new int[] { 0, 0, 0 };
 	private double time = 0;
-	private double[] oldTime = new double[]{0,0,0};
-	private int[] oldTiltData = new int[]{0,0,0};
+	private double[] oldTime = new double[] { 0, 0, 0 };
+	private int[] oldTiltData = new int[] { 0, 0, 0 };
 
 	private double sensitivity = 0.00875;
 
 	/**
 	 * @param brickControl
-	 * 			Association to BrickControl
+	 *            Association to BrickControl
 	 * @param port
-	 * 			the port the Abs_Imu is plugged into
+	 *            the port the Abs_Imu is plugged into
 	 */
 	public Abs_ImuProcessingBrick(BrickControlBrick brickControl, I2CPort port) {
 		abs_imu = new AbsoluteIMU_ACG(port);
@@ -34,50 +34,48 @@ public class Abs_ImuProcessingBrick {
 	}
 
 	/**
-	 * Processes GyroData of given Dimension (into an angle) and sends it to pi-server, if it has changed
+	 * Processes GyroData of given Dimension (into an angle) and sends it to
+	 * pi-server, if it has changed
+	 * 
 	 * @param dimension
-	 * 			dimension to be processed;
-	 * 			0: x
-	 * 			1: y
-	 * 			2: z
+	 *            dimension to be processed; 0:x, 1:y, 2:z
 	 */
-	public void processData_Angle(int dimension){
-		// time since last call gets determined 
+	public void processData_Angle(int dimension) {
+		// time since last call gets determined
 		time = System.currentTimeMillis() - oldTime[dimension];
 		gyro = getGyro();
-		
+
 		// Zero error gets corrected
 		gyro[dimension] -= startGyro[dimension];
-		
-		// seit letzem Messen Ueberschrittenen Winkel bestimmen
+
+		// new angle gets calculated
 		deltaAngle[dimension] += gyro[dimension] * sensitivity;
-		
-		// zu Vorherigem Winkel wird der in der Vergangenen Zeit ueberschrittene Winkel hinzuaddiert
 		angle[dimension] += deltaAngle[dimension] / (1000 / time);
-		
-		// bilden des Winkels ohne Periodizitaet
+
+		// angleExcludingPeriodicity gets calculated
 		angleExcludingPeriodicity = angle[dimension] % 360;
-		
-		// neuer Winkel wird nur verschickt, wenn er mindestens 3 Grad vom letzten abweicht
-		if(angleExcludingPeriodicity <= oldAngleExcludingPeriodicity[dimension]-3 || angleExcludingPeriodicity >= oldAngleExcludingPeriodicity[dimension]+3){
+
+		// new angle will only be sent away, if it has changed by +- 3 Degrees
+		if (angleExcludingPeriodicity <= oldAngleExcludingPeriodicity[dimension] - 3
+				|| angleExcludingPeriodicity >= oldAngleExcludingPeriodicity[dimension] + 3) {
 			brickControl.sendData(5, dimension + 16, angleExcludingPeriodicity);
 			oldAngleExcludingPeriodicity[dimension] = angleExcludingPeriodicity;
 		}
 		oldTime[dimension] = System.currentTimeMillis();
 	}
-	
+
 	/**
-	 * Processes TiltData of given dimension and sends it to pi-server, if it has changed
+	 * Processes TiltData of given dimension and sends it to pi-server, if it
+	 * has changed
+	 * 
 	 * @param dimension
-	 * 			dimension to be processed;
-	 * 			0: x
-	 * 			1: y
-	 * 			2: z
+	 *            dimension to be processed; 0:x, 1:y, 2:z
 	 */
-	public void processData_TiltData(int dimension){
+	public void processData_TiltData(int dimension) {
 		int[] TiltData = abs_imu.getTiltData();
-		// neue TiltData wird nur verschickt, wenn sie vom letzten Wert abweicht
-		if(TiltData[dimension] < oldTiltData[dimension] || TiltData[dimension] > oldTiltData[dimension]){
+		// new TiltData will only be sent away, if it has changed
+		if (TiltData[dimension] < oldTiltData[dimension]
+				|| TiltData[dimension] > oldTiltData[dimension]) {
 			brickControl.sendData(5, dimension + 11, TiltData[dimension]);
 			oldTiltData[dimension] = TiltData[dimension];
 		}
