bc681ca6b233c9393f80875c6b69c005d05bc72c
Jan <jankrebes@gmail.com>
2014-12-30 00:35:13 +0100

    Odometer beinahe vervollstaendigt

--- pi-server/src/de/null_pointer/navigation/map/Odometer.java
+++ pi-server/src/de/null_pointer/navigation/map/Odometer.java
@@ -1,14 +1,99 @@
 package de.null_pointer.navigation.map;
 
+import de.null_pointer.sensorprocessing_pi.Abs_ImuProcessingPi;
+import de.null_pointer.sensorprocessing_pi.AccumulatorProcessingPi;
+
 public class Odometer {
 
+	private AccumulatorProcessingPi accumulator = null;
+	private Abs_ImuProcessingPi abs_imu = null;
+
 	private double distanceCounter = 0;
-	
-	public  Odometer(){
-		
+
+	private double wheelRadius = 0;
+	private int milliVolt = 0;
+	private int angle;
+
+	private double tmpDistance = 0;
+
+	public Odometer(AccumulatorProcessingPi accumulator,
+			Abs_ImuProcessingPi abs_imu, int wheelRadius) {
+		this.accumulator = accumulator;
+		this.abs_imu = abs_imu;
+
+		this.wheelRadius = wheelRadius;
 	}
-	
+
+	/**
+	 * Calculates distance traveled in the given time period and adds it to the
+	 * distance Counter
+	 * 
+	 * @param time
+	 *            time in milliseconds
+	 * @param currentSpeed
+	 *            current speed of the Robot in degrees per second
+	 */
+	public void calculateDistance(double time, int currentSpeed) {
+		milliVolt = accumulator.getMilliVolt();
+		angle = deviationAngle();
+
+		// TODO Batteriestatus beachten
+		tmpDistance = (currentSpeed / 360) * 2 * Math.PI * wheelRadius
+				* (time / 1000);
+		distanceCounter += tmpDistance * Math.cos(angle);
+
+	}
+
+	/**
+	 * calculates the deviation from the ideal angle
+	 * 
+	 * @return int
+	 */
+	private int deviationAngle() {
+		int heading = abs_imu.getHeading();
+		int angle = abs_imu.getAngleHorizontal();
+
+		switch (heading) {
+		case 0:
+			if (angle <= 45) {
+				return 45;
+			} else {
+				return (360 - angle);
+			}
+		case 1:
+			if (angle <= 90) {
+				return (90 - angle);
+			} else {
+				return (angle - 90);
+			}
+		case 2:
+			if (angle <= 180) {
+				return (180 - angle);
+			} else {
+				return (angle - 180);
+			}
+		case 4:
+			if (angle <= 270) {
+				return (270 - angle);
+			} else {
+				return (angle - 270);
+			}
+		default:
+			break;
+		}
+		return 0;
+	}
+
 	public double getDistanceCounter() {
 		return distanceCounter;
 	}
+
+	public void setDistanceCounteR(int value) {
+		distanceCounter = value;
+	}
+
+	public void resetDistanceCounter() {
+		distanceCounter = 0;
+	}
+
 }
