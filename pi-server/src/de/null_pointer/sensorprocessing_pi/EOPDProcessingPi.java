98d990c6ca634a11a9701aeb31d56041e325b6a8
Sebastian Bischoff <sebastian@salzreute.de>
2015-01-17 14:22:06 +0100

    Umrechnung der EOPD-Werte durch verschiedene Funktionen hinzugefuegt
    Die Funktionen wurden durch Regression von Messwerten aufgestellt

--- pi-server/src/de/null_pointer/sensorprocessing_pi/EOPDProcessingPi.java
+++ pi-server/src/de/null_pointer/sensorprocessing_pi/EOPDProcessingPi.java
@@ -14,8 +14,17 @@ public class EOPDProcessingPi {
 
 	public void setEOPDdistance(int value) {
 		synchronized (lockDistance) {
-			// Wert zur Streckeneinteilung ggf. anpassen
-			distance = 150 / Math.sqrt(value);
+			if (value < 520) {
+				distance = 0.00625 * value + 0.1875;
+			} else if (value < 890) {
+				distance = 8.865467626 + (1.007194245E-14) * Math
+						.sqrt((8.792541276E+29) - (9.928571429E+26) * value);
+			} else if (value < 970) {
+				distance = 0.07918552036 * value - 62.72322775;
+			} else {
+				distance = 29.27093359 - (5.497593840E-14) * Math
+						.sqrt((1.840381553E+30) - (1.818977591E+27) * value);
+			}
 		}
 	}
 }
