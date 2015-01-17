490aa1f08bcc1a97c9fd408d955ef95005141570
Sebastian Bischoff <sebastian@salzreute.de>
2015-01-17 21:48:47 +0100

    Behavior BlackTile weitergeschrieben

--- pi-server/src/de/null_pointer/behavior/BlackTile.java
+++ pi-server/src/de/null_pointer/behavior/BlackTile.java
@@ -1,10 +1,13 @@
 package de.null_pointer.behavior;
 
+import java.util.Properties;
+
 import org.apache.log4j.Logger;
 
 import de.null_pointer.communication_pi.BrickControlPi;
 import de.null_pointer.motorcontrol_pi.MotorControlPi;
 import de.null_pointer.navigation.map.Navigation;
+import de.null_pointer.navigation.map.Odometer;
 import de.null_pointer.sensorprocessing_pi.Abs_ImuProcessingPi;
 import de.null_pointer.sensorprocessing_pi.LSAProcessingPi;
 import lejos.robotics.subsumption.Behavior;
@@ -12,17 +15,29 @@ import lejos.robotics.subsumption.Behavior;
 public class BlackTile implements Behavior {
 	private static Logger logger = Logger.getLogger(BlackTile.class);
 
-	MotorControlPi motorControl;
-	LSAProcessingPi lsa;
-	Abs_ImuProcessingPi absImu;
-	Navigation nav;
+	private Properties propPiServer = null;
+
+	private MotorControlPi motorControl = null;
+	private LSAProcessingPi lsa = null;
+	private Abs_ImuProcessingPi absImu = null;
+	private Navigation nav = null;
+	private Odometer odometer = null;
+	private boolean moving = false;
+	private long time = -1;
+	private int speed = -1;
 
 	public BlackTile(MotorControlPi motorControl, LSAProcessingPi lsa,
-			Abs_ImuProcessingPi absImu, Navigation nav) {
+			Abs_ImuProcessingPi absImu, Navigation nav, Odometer odometer,
+			Properties propPiServer) {
 		this.motorControl = motorControl;
 		this.lsa = lsa;
 		this.absImu = absImu;
 		this.nav = nav;
+		this.odometer = odometer;
+		this.propPiServer = propPiServer;
+
+		speed = Integer.parseInt(propPiServer
+				.getProperty("Behavior.BlackTile.speed"));
 	}
 
 	@Override
@@ -33,19 +48,33 @@ public class BlackTile implements Behavior {
 
 	@Override
 	public void action() {
+		time = 0;
+		moving = true;
 		logger.info("Schwarze Kachel erkannt");
 		motorControl.stop();
 		nav.setBlackTile();
-		// TODO: Zurueck fahren
+		
+		motorControl.backward(speed);
+		while (moving && odometer.getDistanceCounter() > 0) {
+			odometer.calculateDistance(time, -speed);
+			time = System.currentTimeMillis();
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				logger.fatal("InterruptedException while sleep()");
+			}
+			time = System.currentTimeMillis() - time;
+		}
+		motorControl.stop();
 		int directionToMove = nav.tremauxAlgorithm(absImu.getHeading(), true);
 
-		// TODO: in "directionToMove"-Richtung fahren
+		// TODO: in "directionToMove"-Richtung drehen
 
 	}
 
 	@Override
 	public void suppress() {
-		// TODO Auto-generated method stub
+		moving = false;
 
 	}
 
