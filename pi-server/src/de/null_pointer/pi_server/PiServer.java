6ab43f8ad19bbd396ca65f72b133730c1d224974
Sebastian Bischoff <sebastian@salzreute.de>
2015-02-01 13:15:41 +0100

    Bugfix #2 (ArrayIndexOutOfBounds)

--- pi-server/src/de/null_pointer/pi_server/PiServer.java
+++ pi-server/src/de/null_pointer/pi_server/PiServer.java
@@ -44,7 +44,13 @@ public class PiServer {
 			s = args[i];
 			// ruft die GUI auf
 			if (s.equals("-gui")) {
-				if (args[i + 1] == null) {
+				i++;
+				s = args[i];
+				if (s.equals("navigation")) {
+					logger.debug("Starte Navigation-GUI");
+					GuiNavigation navGUI = new GuiNavigation();
+					logger.info("Navigation-GUI gestartet");
+				} else if (s.equals("normal")) {
 					logger.debug("Starte GUI");
 					HandleValues vGUI = new HandleValues(initProgram.getLsa(),
 							initProgram.getAbsImu(), initProgram.getEopdLeft(),
@@ -52,14 +58,6 @@ public class PiServer {
 							initProgram.getDistNx(), initProgram.getThermal());
 					logger.info("GUI gestartet");
 					vGUI.start();
-				} else {
-					i++;
-					s = args[i];
-					if (s.equals("navigation")) {
-						logger.debug("Starte Navigation-GUI");
-						GuiNavigation navGUI = new GuiNavigation();
-						logger.info("Navigation-GUI gestartet");
-					}
 				}
 			}
 
