cb55d54e3520308959acb1b22651d9fcf6cd8681
Jan Krebes <jankrebes@gmail.com>
2015-02-11 12:35:48 +0100

    action-Methode des Victim-Behavior implementiert; Initialisierung dessen angepasst (brickCon wird benoetigt)

--- pi-server/src/de/null_pointer/behavior/Victim.java
+++ pi-server/src/de/null_pointer/behavior/Victim.java
@@ -6,12 +6,14 @@ import lejos.robotics.subsumption.Behavior;
 
 import org.apache.log4j.Logger;
 
+import de.null_pointer.communication_pi.BrickControlPi;
 import de.null_pointer.motorcontrol_pi.MotorControlPi;
 import de.null_pointer.sensorprocessing_pi.ThermalSensorProcessingPi;
 
 public class Victim implements Behavior {
 	private static Logger logger = Logger.getLogger(Victim.class);
 
+	private BrickControlPi brickControlLED = null;
 	private MotorControlPi motorControl = null;
 	private ThermalSensorProcessingPi thermal = null;
 	private Properties propPiServer = null;
@@ -19,8 +21,11 @@ public class Victim implements Behavior {
 	private int minimumTemperature = -1;
 	private int maximumTemperature = -1;
 
-	public Victim(MotorControlPi motorControl,
+	private int victimsFound = 0;
+
+	public Victim(BrickControlPi brickControlLED, MotorControlPi motorControl,
 			ThermalSensorProcessingPi thermal, Properties propPiServer) {
+		this.brickControlLED = brickControlLED;
 		this.motorControl = motorControl;
 		this.thermal = thermal;
 		this.propPiServer = propPiServer;
@@ -47,10 +52,15 @@ public class Victim implements Behavior {
 	@Override
 	public void action() {
 		logger.info("Victim detected");
-		// LED blinken lassen
-		// // Rettungspaket abwerfen
-		// TODO: Victim-Zaehler erhoehen
 
+		// TODO: ggf. bis zur mitte der Kachel fahren (mittels Odometer)
+		motorControl.stop();
+
+		brickControlLED.blinkColorSensorLED();
+
+		// TODO: Rettungspaket abwerfen
+
+		victimsFound++;
 	}
 
 	@Override
@@ -59,4 +69,7 @@ public class Victim implements Behavior {
 
 	}
 
+	public int getVictimsFound() {
+		return victimsFound;
+	}
 }
