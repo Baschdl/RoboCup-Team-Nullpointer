faa187280b806a46dd758e51eeeba90f4ec40b56
lfkat <Scherer-Samuel@T-Online.de>
2015-02-28 19:43:19 +0100

    synchronized-Methoden auf lock-Objekt umgestellt

--- pi-server/src/de/null_pointer/motorcontrol_pi/Semaphore.java
+++ pi-server/src/de/null_pointer/motorcontrol_pi/Semaphore.java
@@ -4,30 +4,35 @@ import lejos.robotics.subsumption.Arbitrator;
 
 public class Semaphore {
 	private Arbitrator arbitrator = null;
+	private final Object lock = new Object();
 
-	int isBlocked = 0;
+	private int blockedMotors = 0;
 
 	public Semaphore(Arbitrator arbitrator) {
 		this.arbitrator = arbitrator;
 	}
 
-	public synchronized void up() {
-		isBlocked++;
-		if (isBlocked == 1) {
-			try {
-				arbitrator.wait();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	public void up() {
+		synchronized (lock) {
+			blockedMotors++;
+			if (blockedMotors == 1) {
+				try {
+					arbitrator.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 
 	}
 
-	public synchronized void down() {
-		isBlocked--;
-		if (isBlocked == 0) {
-			arbitrator.notify();
+	public void down() {
+		synchronized (lock) {
+			blockedMotors--;
+			if (blockedMotors == 0) {
+				arbitrator.notify();
+			}
 		}
 	}
 
